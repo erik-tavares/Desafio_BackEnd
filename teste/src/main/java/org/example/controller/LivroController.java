46838d82bf62bd9f88ac2a15172a9d976576cf11
Erik Tavares <96538153+erik-tavares@users.noreply.github.com>
2024-04-10 12:03:40 -0300

    Update LivroController.java

--- teste/src/main/java/org/example/controller/LivroController.java
+++ teste/src/main/java/org/example/controller/LivroController.java
@@ -40,28 +40,20 @@ public class LivroController {
             @RequestParam(required = false) String sobrenomeAutor) {
 
         if (titulo != null && generoLiterario != null && sobrenomeAutor != null) {
-            // Se todos os três parâmetros estiverem presentes
             return livroService.listarLivrosPorTituloGeneroESobrenome(titulo, generoLiterario, sobrenomeAutor);
         } else if (titulo != null && generoLiterario != null) {
-            // Se título e gênero literário estiverem presentes
             return livroService.listarLivrosPorTituloEGenero(titulo, generoLiterario);
         } else if (titulo != null && sobrenomeAutor != null) {
-            // Se título e sobrenome do autor estiverem presentes
             return livroService.listarLivrosPorTituloESobrenome(titulo, sobrenomeAutor);
         } else if (generoLiterario != null && sobrenomeAutor != null) {
-            // Se gênero literário e sobrenome do autor estiverem presentes
             return livroService.listarLivrosPorGeneroESobrenome(generoLiterario, sobrenomeAutor);
         } else if (titulo != null) {
-            // Se apenas o título estiver presente
             return livroService.listarLivrosPorTitulo(titulo);
         } else if (generoLiterario != null) {
-            // Se apenas o gênero literário estiver presente
             return livroService.listarLivrosPorGenero(generoLiterario);
         } else if (sobrenomeAutor != null) {
-            // Se apenas o sobrenome do autor estiver presente
             return livroService.listarLivrosPorSobrenomeAutor(sobrenomeAutor);
         } else {
-            // Se nenhum parâmetro estiver presente, lista todos os livros
             return livroService.listarTodosLivros();
         }
     }
